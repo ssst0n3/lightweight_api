e52a228c37a1311721cbc0590b8809cae0b9f041
st0n3 <ssst0n3@gmail.com>
2020-06-02 11:45:43 +0800

    update

--- response.go
+++ response.go
@@ -11,8 +11,16 @@ func Response200CreateSuccess(c *gin.Context, id uint) {
 	})
 }
 
-func Response200DeleteSuccess(c *gin.Context)  {
+func Response200Success(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"success": true,
 	})
+}
+
+func Response200UpdateSuccess(c *gin.Context)  {
+	Response200Success(c)
+}
+
+func Response200DeleteSuccess(c *gin.Context)  {
+	Response200Success(c)
 }
