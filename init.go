cafb134dde07a6d5f13c20a2700cfe4d02bb0641
Lei Wang <ssst0n3@gmail.com>
2021-11-30 17:46:02 +0800

    init => Init

--- init.go
+++ init.go
@@ -5,7 +5,7 @@ import (
 	"github.com/ssst0n3/lightweight_api/test/test_config"
 )
 
-func init() {
+func Init() {
 	if !test_config.IsTestLightweightApi() {
 		awesome_error.CheckFatal(InitGormDB())
 	}
--- init_test.go
+++ init_test.go
@@ -1,15 +1,10 @@
 package lightweight_api
 
 import (
-	"github.com/ssst0n3/awesome_libs/awesome_error"
 	"github.com/ssst0n3/lightweight_api/test/test_config"
 )
 
 func init() {
-	Init()
-}
-
-func Init() {
 	test_config.Init()
-	awesome_error.CheckFatal(InitGormDB())
+	Init()
 }
